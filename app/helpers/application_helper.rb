94516a80398fb19e75f6ad8612fc00b007e3ca1c
Barry Keenan <me@keeny.co.uk>
2012-06-11 22:42:07 +1200

    added sort highlight indicator

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,15 @@
 module ApplicationHelper
-	def sortable(column, title = nil)
+
+	def sortable(column, title = nil, options = {})
+		options[:id] ||= column + '_header'
+		# options[:class] = (column == sort_column)? "hilite #{sort_direction}" : nil
 		title ||= column.titleize
 		direction = column == sort_column && sort_direction == 'asc' ? 'desc' : 'asc'
-		link_to title, movies_path(:sort => column, :direction => direction)
+		link_to title, movies_path(:sort => column, :direction => direction), options
 	end
+	
+	def sort_class(column)
+		(column == sort_column)? "hilite #{sort_direction}" : nil
+	end
+	
 end
